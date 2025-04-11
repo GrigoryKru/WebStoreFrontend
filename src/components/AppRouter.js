23b626cab7cefcd729d52b98fb2c56d96d9b0658
Grigory Krutov <grukrutov@MacBook-Pro-Grigory.local>
2025-04-11 21:46:50 +0300

    add Store for components

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route, Navigate} from 'react-router-dom';
 import { authRoutes, publicRoutes } from '../routes';
 import { SHOP_ROUTE } from '../utils/consts';
+import { Context } from '..';
 
 const AppRouter = () => {
-  const isAuth = false;
+  const {user} = useContext(Context)
+
   return (
     <Routes>
-      {isAuth &&
+      {user.isAuth &&
         authRoutes.map(({ path, Component }) => (
           <Route key={path} path={path} element={<Component />} />
         ))}
