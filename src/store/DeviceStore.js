e3ef7bffb7e83c00a5d9321ea89ff2de9061aa7a
Grigory Krutov <grukrutov@MacBook-Pro-Grigory.local>
2025-04-12 00:08:46 +0300

    add Type and Brand bar

--- src/store/DeviceStore.js
+++ src/store/DeviceStore.js
@@ -5,6 +5,8 @@ export default class DeviceStore {
     this._types = [];
     this._brands = [];
     this._devices = [];
+    this._selectedType = {};
+    this._selectedBrand = {};
     makeAutoObservable(this);
   }
 
@@ -17,6 +19,12 @@ export default class DeviceStore {
   setDevices(devices) {
     this._devices = devices;
   }
+  setSelectedType(type) {
+    this._selectedType = type;
+  }
+  setSelectedBrand(brand) {
+    this._selectedBrand = brand;
+  }
 
   get types() {
     return this._types;
@@ -26,5 +34,11 @@ export default class DeviceStore {
   }
   get devices() {
     return this._devices;
+  }
+  get selectedType() {
+    return this._selectedType;
+  }
+  get selectedBrand() {
+    return this._selectedBrand;
   }
 }
